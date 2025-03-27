ba18541053c96f74c13e7675dc446512683b987b
samuelcney <samuelcney2@gmail.com>
2025-03-27 19:08:25 -0300

    fix: category colors

--- src/utils/categoryColors.ts
+++ src/utils/categoryColors.ts
@@ -1,9 +1,12 @@
 export const categoryColors: Record<string, string> = {
-  guitarra: "#D64E07",
+  guitarra: "#32CD32",
   contrabaixo: "#5A3D31",
   piano: "#1A73E8",
-  timbres: "#FFa100",
+  bateria: "#FFa100",
+  teclado: "#D64E07",
   jazz: "#6A0DAD",
   violão: "#8B4513",
   canto: "#FF1493",
+  rock: "#721121",
+  blues: "#0000CD",
 };
